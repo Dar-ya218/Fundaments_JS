5feb7fbe71f1f8ef683993a25ecf3bc83564e84e
Dar-ya218 <dasharoura13465@gmail.com>
2023-02-02 00:21:05 +0100

    Buscando fallo

--- M5_Est_Repet_Arrays/M5E4_Adivina_Num_5/index.js
+++ M5_Est_Repet_Arrays/M5E4_Adivina_Num_5/index.js
@@ -1,31 +1,30 @@
 function start() {
-    
+
     const randoomNum = Math.ceil(Math.random() * 10);
-    console.log (randoomNum);
+    console.log(randoomNum);
 
-  
-    let userNum = Number (prompt("¡¡¡Introduce un numero!!!"))
+    const MAX_TRIES = 5;
+    let userNum = Number(prompt("¡¡¡Introduce un numero!!!"))
     debugger
-    let result =[]
-
-while (userNum === randoomNum) {
-    document.getElementById("result").innerHTML = `Enhorabuena, el número era ${randoomNum}` 
-    break
-}
-
-   for (let i = 1; i < 5 ; i++) {
-    userNum = Number (prompt("¡¡¡No es correcto, vuelve a intentar!!!")) }
     
-    
-   
 
-    if (userNum !== randoomNum){
-        document.getElementById("result").innerHTML = `¡Has utilizado demasiados intentos! El número era ${randoomNum}`  
+    while (userNum !== randoomNum) {
+        for (let i = 1; i < 5; i++)
+        userNum = Number (prompt("¡¡¡No es correcto, vuelve a intentar!!!"))
+    
+    if (userNum === randoomNum) {
+        document.getElementById("result").innerHTML = `Enhorabuena, el número era ${randoomNum}`
+    
     }
 
-    
+   
 
 
-   }
-    
+    document.getElementById("result").innerHTML = `¡Has utilizado demasiados intentos! El número era ${randoomNum}`
 
+}
+
+
+
+
+}
