d196ebf987fcae75c6b1f946a8abe0fd27d79323
Dar-ya218 <dasharoura13465@gmail.com>
2023-02-06 11:52:29 +0100

    Creado m5ext1

--- M5_Est_Repet_Arrays/M5EXT1/index.js
+++ M5_Est_Repet_Arrays/M5EXT1/index.js
@@ -2,39 +2,32 @@ function game() {
     const randomNumber = Math.ceil(Math.random() * 500);
     console.log(randomNumber);
 
-    let userNumber = Number(prompt("Introduce tu numero: "));
-
-    if (userNumber === randomNumber) {
-        document.getElementById("result").innerHTML = `Falicidades, has adivinado. El numero era ${randomNumber}`
-        // return
-    }
+    let userNumber = Number(prompt("Introduce tu número de 1 a 500:"))
 
     while (userNumber !== randomNumber) {
 
-       
         if (userNumber >= randomNumber + 50) {
             userNumber = Number(prompt("Frío, frío: tu número es mayor. ¡Sigue intentando! "));
-            // return
         }
         if (userNumber <= randomNumber - 50) {
             userNumber = Number(prompt("Frío, frío: tu número es menor. ¡Sigue intentando! "));
-            // return
         }
         if (userNumber >= randomNumber + 15 && userNumber < randomNumber + 50) {
             userNumber = Number(prompt("Tibio, Tibio: tu número es mayor. ¡Sigue intentando! "));
-            //return
         }
         if (userNumber <= randomNumber - 15 && userNumber > randomNumber - 50) {
             userNumber = Number(prompt("Tibio, Tibio: tu número es menor. ¡Sigue intentando! "));
-            //return
         }
         if (userNumber >= randomNumber + 1 && userNumber < randomNumber + 15) {
             userNumber = Number(prompt("Caliente, Caliente: tu número es mayor. ¡Sigue intentando! "));
-            //return
         }
         if (userNumber <= randomNumber - 1 && userNumber > randomNumber + 15) {
             userNumber = Number(prompt("Caliente, Caliente: tu número es menor. ¡Sigue intentando! "));
-            return
         }
+    }
+
+    if (userNumber === randomNumber) {
+        document.getElementById("result").innerHTML = `Felicidades, has adivinado. El número era ${randomNumber}`
+        return
     }
 }
