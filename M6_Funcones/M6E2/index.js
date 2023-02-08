be394c229115e205582d764e99a9dfe99d73876f
Dar-ya218 <dasharoura13465@gmail.com>
2023-02-08 10:17:24 +0100

    añadir if en js

--- M6_Funcones/M6E2/index.js
+++ M6_Funcones/M6E2/index.js
@@ -4,7 +4,11 @@ function askAge() {
     const firstUserAge = Number(document.getElementById("ageFirst").value);
     const secondUserAge = Number(document.getElementById("ageSecond").value);
     const thirdUserAge = Number(document.getElementById("ageThird").value);
-    
+
+    if (firstUserAge > 120 || firstUserAge < 0 || secondUserAge > 120 || secondUserAge < 0 || thirdUserAge > 120 || thirdUserAge < 0) {
+        alert("La edad no es valido")
+        throw Error ("Numero incorecto")
+    }
     
     const result = [firstUserAge, secondUserAge, thirdUserAge]
 
