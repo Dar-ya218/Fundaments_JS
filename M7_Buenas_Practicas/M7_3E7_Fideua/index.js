2122abe7e840b098a218ebbd41c5fb6f9e7fd994
Dar-ya218 <dasharoura13465@gmail.com>
2023-02-12 14:00:50 +0100

    Corecion Fideua

--- M7_Buenas_Practicas/M7_3E7_Fideua/index.js
+++ M7_Buenas_Practicas/M7_3E7_Fideua/index.js
@@ -10,10 +10,10 @@ const noodlsPriceKg = Number(document.getElementById("priceNoodls").value);
 const squidPriceKg = Number(document.getElementById("priceNoodls").value);
 const shrimpPriceKg = Number(document.getElementById("priceNoodls").value);
 
-let message = `${noodlsPerPerson*friends} de noodls ${squidPerPerson*friends} de calamares ${shrimpPerPerson*friends} de gambas`
+let message = `${noodlsPerPerson*friends.toFixed(2)} kg de noodls ${squidPerPerson*friends.toFixed(2)} kg de calamares ${shrimpPerPerson*friends.toFixed(2)} kg de gambas`
 let unitPrice = noodlsPerPerson*noodlsPriceKg + squidPerPerson*squidPriceKg + shrimpPerPerson*shrimpPriceKg
 let totalPrice = unitPrice*friends
-document.getElementById("result").innerHTML = `El precio por persona es ${unitPrice} <br> El precio total es ${totalPrice} <br> La cantidad utilizada es de ${message}`
+document.getElementById("result").innerHTML = `El precio por persona es ${unitPrice.toFixed(2)} <br> El precio total es ${totalPrice.toFixed(2)} <br> La cantidad utilizada es de ${message}`
     
 }
 
